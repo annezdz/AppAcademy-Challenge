c6065a23523b68331ca7e4208d125fbbcb7bf267
annezdz <annezdz@hotmail.com>
2021-05-28 19:53:33 -0300

    Changes in LerCsvFiles and CandidatoController

--- src/main/java/com/premiersoft/appacademy/appacademy/controller/CandidatoController.java
+++ src/main/java/com/premiersoft/appacademy/appacademy/controller/CandidatoController.java
@@ -1,4 +1,21 @@
 package com.premiersoft.appacademy.appacademy.controller;
 
+import com.premiersoft.appacademy.appacademy.utils.LerCsvFiles;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CandidatoController {
+
+    private final LerCsvFiles lerCsvFiles;
+
+    public CandidatoController(LerCsvFiles lerCsvFiles) {
+        this.lerCsvFiles = lerCsvFiles;
+    }
+
+    @GetMapping("api/lista")
+    public ResponseEntity<?> ler(){
+       return ResponseEntity.ok(lerCsvFiles.readCsvLineByLine("candidatos.csv"));
+    }
 }
