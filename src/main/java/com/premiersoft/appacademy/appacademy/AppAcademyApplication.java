8b6974c8e6b5352510d1e9785e63d90d2974472d
annezdz <annezdz@hotmail.com>
2021-05-28 09:24:15 -0300

    Initial commit

---
+++ src/main/java/com/premiersoft/appacademy/appacademy/AppAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.premiersoft.appacademy.appacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAcademyApplication.class, args);
+	}
+
+}
